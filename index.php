35beec8421d9e9a76e3a190c43d4a199c0361e81
Task Nagashige <task.19.xx@gmail.com>
2020-06-01 19:05:19 +0900

    入力フォームの作成

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
+<html>
+<head><title>掲示板App</title></head>
+<body>
 
-echo "Hello, World!";
+<h1>掲示板App</h1>
+
+<h2>投稿フォーム</h2>
+
+<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+    <input type="text" name="personal_name" placeholder="名前"><br><br>
+    <textarea name="contents" rows="8" cols="40" placeholder="内容">
+</textarea><br><br>
+    <input type="submit" name="btn1" value="投稿する">
+</form>
+
+<h2>スレッド</h2>
+
+</body>
+</html>
