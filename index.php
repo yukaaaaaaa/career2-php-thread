dd971a411bd1fc6b66f2663d04ffd1301b61e0b0
Task Nagashige <task.19.xx@gmail.com>
2020-06-10 02:12:02 +0900

    gitignoreに追加

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <h2>投稿フォーム</h2>
 
-<form method="POST">
+<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
     <input type="text" name="personal_name" placeholder="名前" required><br><br>
     <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
 </textarea><br><br>
@@ -15,9 +15,56 @@
 
 <h2>スレッド</h2>
 
-</body>
-</html>
-
 <?php
 
-echo "職業実践2 - 練習用App";
+const THREAD_FILE = 'thread.txt';
+
+function readData() {
+    // ファイルが存在しなければデフォルト空文字のファイルを作成する
+    if (! file_exists(THREAD_FILE)) {
+        $fp = fopen(THREAD_FILE, 'w');
+        fwrite($fp, '');
+        fclose($fp);
+    }
+
+    $thread_text = file_get_contents(THREAD_FILE);
+    echo $thread_text;
+}
+
+function writeData() {
+    $personal_name = $_POST['personal_name'];
+    $contents = $_POST['contents'];
+    $contents = nl2br($contents);
+
+    $data = "<hr>\n";
+    $data = $data."<p>投稿者:".$personal_name."</p>\n";
+    $data = $data."<p>内容:</p>\n";
+    $data = $data."<p>".$contents."</p>\n";
+
+    $fp = fopen(THREAD_FILE, 'a');
+
+    if ($fp){
+        if (flock($fp, LOCK_EX)){
+            if (fwrite($fp,  $data) === FALSE){
+                print('ファイル書き込みに失敗しました');
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+        }
+    }
+
+    fclose($fp);
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    writeData();
+}
+
+readData();
+
+?>
+
+</body>
+</html>
