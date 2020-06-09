8bb42c162b3e534c23076e6cce135feb0dc597da
Task Nagashige <task.19.xx@gmail.com>
2020-06-10 02:16:52 +0900

    ブラウザのリロード対策を組み込んだ

--- index.php
+++ index.php
@@ -56,6 +56,11 @@ function writeData() {
     }
 
     fclose($fp);
+
+    // ブラウザのリロード対策
+    $redirect_url = $_SERVER['HTTP_REFERER'];
+    header("Location: $redirect_url");
+    exit;
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
