6153e98deba9be43e7cd446174ebe8c0693741cb
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-06-17 11:04:22 +0900

    削除機能の追加

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
+
 <head><title>掲示板App</title></head>
+
 <body>
 
 <h1>掲示板</h1>
@@ -11,32 +13,45 @@
     <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
 </textarea><br><br>
     <input type="submit" name="btn" value="投稿する">
+
+</form>
+<h2>スレッド</h2>
+
+<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+    <input type="hidden" name="method" value="DELETE">
+    <button type="submit">投稿を全削除する</button>
 </form>
 
-<h2>スレッド</h2>
+
 
 <?php
 
 date_default_timezone_set('Asia/Tokyo');
 const THREAD_FILE = 'thread.txt';
 
+
+
 function readData() {
+
     // ファイルが存在しなければデフォルト空文字のファイルを作成する
     if (! file_exists(THREAD_FILE)) {
         $fp = fopen(THREAD_FILE, 'w');
         fwrite($fp, '');
         fclose($fp);
-    }
 
+    }
     $thread_text = file_get_contents(THREAD_FILE);
     echo $thread_text;
 }
 
+
+
 function writeData() {
+
     $personal_name = $_POST['personal_name'];
     $contents = $_POST['contents'];
     $contents = nl2br($contents);
-
+    
     $data = "<hr>\n";
     $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
     $data = $data."<p>投稿者:".$personal_name."</p>\n";
@@ -44,7 +59,6 @@ function writeData() {
     $data = $data."<p>".$contents."</p>\n";
 
     $fp = fopen(THREAD_FILE, 'a');
-    
 
     if ($fp){
         if (flock($fp, LOCK_EX)){
@@ -57,19 +71,25 @@ function writeData() {
             print('ファイルロックに失敗しました');
         }
     }
-
     fclose($fp);
+}
 
+function deleteData() {
+    file_put_contents(THREAD_FILE, "");
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
+        deleteData();
+    } else {
+        writeData();
+    }
     // ブラウザのリロード対策
     $redirect_url = $_SERVER['HTTP_REFERER'];
     header("Location: $redirect_url");
     exit;
 }
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    writeData();
-}
-
 readData();
 
 ?>
